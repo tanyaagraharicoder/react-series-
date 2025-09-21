0157c640718654cb63a45f466e82dcfd1337a994
Tanya Agrahari <rg96958227@gmail.com>
2025-09-21 21:55:19 +0530

    making of componenet

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,26 @@ function App(){
   return (
     
       <div>
-        <h1>hello react </h1>
-        <h1> code step by step </h1>
-        <Header/>
+        <h1>first component</h1>
+      <Fruit/>
+      <Color/>
       </div>
 
     )
 
-  
+}
+function Fruit(){
+  return (
+    <div>
+      <h1> Apple </h1>
+    </div>
+  )
+}
+function Color(){
+  return (
+    <div>
+      <h1> red color </h1>
+    </div>
+  )
 }
 export default App;
